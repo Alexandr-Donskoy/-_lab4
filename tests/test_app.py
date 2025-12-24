b5d2d51208b0bcd06a2702ab789deb2c0d8c77ca
Alexandr-Donskoy <ae.donskoy@gmail.com>
2025-12-24 03:45:27 +0300

    Финальная версия лабораторной работы №4: CI/CD пайплайн

---
+++ tests/test_app.py
@@ -0,0 +1,17 @@
+﻿import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_hello_route(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b'Hello from CI/CD Pipeline' in response.data
+
+def test_health_route(client):
+    response = client.get('/health')
+    assert response.status_code == 200
+    assert b'healthy' in response.data
